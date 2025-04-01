d3816250be4ee3329829b3177e0b8e07a86e7ff9
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2025-04-02 02:14:39 +0530

    updated mailservice

--- mailService.js
+++ mailService.js
@@ -2,15 +2,13 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST || "smtp.gmail.com",
-    port: process.env.EMAIL_PORT || 587,
-    secure: process.env.EMAIL_SECURE === "true", 
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
 
 const sendMail = async ({ to, subject, text }) => {
     try {
