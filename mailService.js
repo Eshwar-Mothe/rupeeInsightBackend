23ef2cbffedf5a6ced9e81555e0afc542d2818b3
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2025-04-02 02:31:23 +0530

    updated mailservice

--- mailService.js
+++ mailService.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
-    auth: {
+    eauth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
