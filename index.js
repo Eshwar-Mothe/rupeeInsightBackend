4a72b7acbc6b8e63e3fa29e4946e6c0fc6782d19
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2025-04-02 00:42:09 +0530

    updated port

--- index.js
+++ index.js
@@ -200,5 +200,5 @@ mongoose.connect(process.env.MONGO_URI, {
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("MongoDB Connection Error:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.Port || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
