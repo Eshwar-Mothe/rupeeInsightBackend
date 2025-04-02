cafef8717bcb5786719343f198864767761983a2
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2025-04-02 15:05:47 +0530

    updated mailservice

--- mailService.js
+++ mailService.js
@@ -7,8 +7,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true, 
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: 'rupeeinsight@gmail.com',
+        pass: 'pwzr ddeq hmht noqz',
     },
 });
 
