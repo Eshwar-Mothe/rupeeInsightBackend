7771bd6b822c48152306073dbf4499b8b423b62b
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2025-04-03 02:56:59 +0530

    updated functionality

--- index.js
+++ index.js
@@ -174,3 +174,4 @@ mongoose.connect(process.env.MONGO_URI, { dbName: "usersData" })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
