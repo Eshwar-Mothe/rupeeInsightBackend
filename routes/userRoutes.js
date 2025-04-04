5479b31376536c4753b7e45d43ab25f20c63537f
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2025-04-04 12:56:55 +0530

    added functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -21,6 +21,7 @@ router.post("/signup", upload.single("profileImage"), addUser);
 router.post("/reminders", setReminder)
 router.post("./expenses", addExpense)
 router.post("./loans", addDebt)
+
 // router.put("/users/:id", async (req, res) => {
 //     const { id } = req.params;
 //     const { name, email, profileImage } = req.body;
@@ -42,6 +43,19 @@ router.post("./loans", addDebt)
 //         res.status(500).json({ message: "Internal server error" });
 //     }
 // });
+
+// module.exports = router;
+
+router.get('/:userId/totals', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        res.json(user.totals);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 // module.exports = router;
 
