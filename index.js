07172f676dcc54be7c0edfa256f94adc11c6a151
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2025-04-10 15:09:43 +0530

    added functionality

--- index.js
+++ index.js
@@ -51,6 +51,19 @@ app.post("/sendMail", async (req, res) => {
     }
 });
 
+app.post("/reset", async (req, res) => {
+    try {
+        const { to, subject, text } = req.body;
+        const isUserExist = await User.findOne({ email: to });
+        if (!isUserExist) return res.status(400).json({ message: "User not exists" });
+
+        const response = await sendMail({ to, subject, text });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to send email." });
+    }
+});
+
 app.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
